74ee2b52030f423ff35c64d80d38939c6746fe2a
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-09-05 13:16:38 -0400

    enables dynamic rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-
   integrations: [icon()],
   scopedStyleStrategy: "class",
+  adapter: netlify(),
+  image: { domains: ["251195.xyz"] },
 });
