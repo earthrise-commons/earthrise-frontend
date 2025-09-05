82daeacc6987045b1bb0c5453e9df0077584403c
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-09-05 13:23:33 -0400

    switches to fully server-rendered pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   prefetch: true,
   integrations: [icon()],
   scopedStyleStrategy: "class",
