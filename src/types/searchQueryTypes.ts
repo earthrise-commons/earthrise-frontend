33e495053fb186f2f160e897c770be38ff5ca3d2
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-11-12 18:06:54 -0500

    adds graphics to filter controls

--- src/types/searchQueryTypes.ts
+++ src/types/searchQueryTypes.ts
@@ -3,9 +3,35 @@ export interface SearchQueryType {
   page: string;
   types: string[];
 }
+
+import articles from "../assets/searchFilters/articles.svg";
+import chapters from "../assets/searchFilters/chapters.svg";
+import lessons from "../assets/searchFilters/lessons.svg";
+import people from "../assets/searchFilters/people.svg";
+
 export const pageTypes = [
-  { label: "Articles", value: "article", color: "var(--color-category-a)" },
-  { label: "Chapter", value: "chapter", color: "var(--color-category-b)" },
-  { label: "Lesson", value: "lesson", color: "var(--color-category-c)" },
-  { label: "Person", value: "person", color: "var(--color-category-d)" },
+  {
+    label: "Articles",
+    value: "article",
+    color: "var(--color-category-a)",
+    Graphic: articles,
+  },
+  {
+    label: "Chapter",
+    value: "chapter",
+    color: "var(--color-category-b)",
+    Graphic: chapters,
+  },
+  {
+    label: "Lesson",
+    value: "lesson",
+    color: "var(--color-category-c)",
+    Graphic: lessons,
+  },
+  {
+    label: "Person",
+    value: "person",
+    color: "var(--color-category-d)",
+    Graphic: people,
+  },
 ];
